70593f306ca3e03132ec6f4ef98b4ae11f92304d
hkboy012090 <mywebsite012090@gmail.com>
2026-07-13 18:32:49 +0800

    Add mobile button handlers for authentication

--- my-website/js/auth.js
+++ my-website/js/auth.js
@@ -4,24 +4,24 @@ import {
   signOut
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
-const loginBtn = document.getElementById("loginBtn");
-const registerBtn = document.getElementById("registerBtn");
-const logoutBtn = document.getElementById("logoutBtn");
+const loginBtnMobile = document.getElementById("loginBtnMobile");
+const registerBtnMobile = document.getElementById("registerBtnMobile");
+const logoutBtnMobile = document.getElementById("logoutBtnMobile");
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    if (loginBtn) loginBtn.style.display = "none";
-    if (registerBtn) registerBtn.style.display = "none";
-    if (logoutBtn) logoutBtn.style.display = "inline-block";
+    if (loginBtnMobile) loginBtnMobile.style.display = "none";
+    if (registerBtnMobile) registerBtnMobile.style.display = "none";
+    if (logoutBtnMobile) logoutBtnMobile.style.display = "block";
   } else {
-    if (loginBtn) loginBtn.style.display = "inline-block";
-    if (registerBtn) registerBtn.style.display = "inline-block";
-    if (logoutBtn) logoutBtn.style.display = "none";
+    if (loginBtnMobile) loginBtnMobile.style.display = "block";
+    if (registerBtnMobile) registerBtnMobile.style.display = "block";
+    if (logoutBtnMobile) logoutBtnMobile.style.display = "none";
   }
 });
 
-if (logoutBtn) {
-  logoutBtn.addEventListener("click", async () => {
+if (logoutBtnMobile) {
+  logoutBtnMobile.addEventListener("click", async () => {
     await signOut(auth);
     alert("Logged out successfully!");
     window.location.href = "login.html";
