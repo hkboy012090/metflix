9ece87b44dd246f99b29089676732b04aaea284d
hkboy012090 <mywebsite012090@gmail.com>
2026-07-14 10:30:28 +0800

    Update Firebase SDK version and config
    Updated Firebase SDK imports and configuration details.

--- my-website/js/firebase.js
+++ my-website/js/firebase.js
@@ -1,32 +1,15 @@
-// Firebase SDK
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
-
-// Firebase Config
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-
-apiKey: "AIzaSyBMTzMvEFUEGSMtBiT8hf5gj6ZocikC-lo",
-
-authDomain: "metflix-d10a6.firebaseapp.com",
-
-projectId: "metflix-d10a6",
-
-storageBucket: "metflix-d10a6.firebasestorage.app",
-
-messagingSenderId: "459321141412",
-
-appId: "1:459321141412:web:873b9addd6d2ae59855e31"
-
+  apiKey: "AIzaSyCprwxcwbJm5Qu-IjVQxKxBuseakVu16dY",
+  authDomain: "metflix-e8145.firebaseapp.com",
+  projectId: "metflix-e8145",
+  storageBucket: "metflix-e8145.firebasestorage.app",
+  messagingSenderId: "995899651117",
+  appId: "1:995899651117:web:3544fba402b658d3f5b4a4",
+  measurementId: "G-QFSQ2PLK7R"
 };
 
-// Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-
-export { auth };
-
+export const auth = getAuth(app);
