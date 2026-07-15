5012f21e4731ae48896741f41678c0c3526f278b
hkboy012090 <mywebsite012090@gmail.com>
2026-07-15 11:14:41 +0800

    Refactor home.js for improved functionality and clarity

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -131,11 +131,18 @@ const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
     }
 
     init();
+const loginBtn = document.getElementById("loginBtn");
 const logoutBtn = document.getElementById("logoutBtn");
 
-if (logoutBtn) {
-  logoutBtn.addEventListener("click", logout);
+if (localStorage.getItem("loggedIn")) {
+    loginBtn.style.display = "none";
+    logoutBtn.style.display = "inline-block";
+} else {
+    loginBtn.style.display = "inline-block";
+    logoutBtn.style.display = "none";
 }
+
+logoutBtn.addEventListener("click", logout);
 const savedMovie = sessionStorage.getItem("selectedMovie");
 
 if (savedMovie && localStorage.getItem("loggedIn")) {
