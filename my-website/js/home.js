a739ae38ba8440d4fe68833aa6922a0644bff2e6
hkboy012090 <mywebsite012090@gmail.com>
2026-07-15 15:54:17 +0800

    Update API key and refactor fetch functions

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,5 +1,5 @@
 import { logout, checkAuth } from "./auth.js";
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '85d06918f5f2d578fd2048c5841b6ee2';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
